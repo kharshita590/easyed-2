02c68e5a90d182042057b012385f0c9f92a926b1
dubey0613 <dubey.sarthak0613@gmail.com>
2024-01-16 19:45:05 +0530

    privacy policy updated

--- src/Pages/Policy.jsx
+++ src/Pages/Policy.jsx
@@ -7,136 +7,125 @@ const Policy = () => {
 
 
     <>
-      <Navbar/>
+      <Navbar />
       {/* <Sidebar/> */}
-    
-    <div className="container mx-auto mt-20 px-4">
-      <h1 className="text-4xl font-bold mb-8">Privacy Policy</h1>
-      <p className="text-sm text-gray-500 mb-4">Last updated on 03-11-2023 17:50:55</p>
 
-      <p className="mb-4">
-        This privacy policy (“Policy”) relates to the manner EASYED EDUVERSE PRIVATE LIMITED (“we”, “us”, “our”) in which
-        we use, handle and process the data that you provide us in connection with using the products or services we
-        offer. By using this website or by availing goods or services offered by us, you agree to the terms and
-        conditions of this Policy, and consent to our use, storage, disclosure, and transfer of your information or data
-        in the manner described in this Policy.
-      </p>
-
-      <p className="mb-4">
-        We are committed to ensuring that your privacy is protected in accordance with applicable laws and regulations.
-        We urge you to acquaint yourself with this Policy to familiarize yourself with the manner in which your data is
-        being handled by us.
-      </p>
-
-      <p className="mb-4">
-        EASYED EDUVERSE PRIVATE LIMITED may change this Policy periodically and we urge you to check this page for the
-        latest version of the Policy in order to keep yourself updated.
-      </p>
-
-      <h2 className="text-2xl font-bold my-4">What data is being collected</h2>
-
-      <p className="mb-4">
-        We may collect the following information from you:
-      </p>
-
-      <ul className="list-disc list-inside mb-4">
-        <li>Name</li>
-        <li>Contact information including address and email address</li>
-        <li>Demographic information or, preferences or interests</li>
-        <li>Personal Data or Other information relevant/required for providing the goods or services to you</li>
-      </ul>
-
-      <p className="mb-4">
-        The meaning of Personal Data will be as defined under relevant Indian laws. Note: Notwithstanding anything under
-        this Policy as required under applicable Indian laws, we will not be storing any credit card, debit card or any
-        other similar card data of yours. Please also note that all data or information collected from you will be
-        strictly in accordance with applicable laws and guidelines.
-      </p>
-
-      <h2 className="text-2xl font-bold my-4">What we do with the data we gather</h2>
-
-      <p className="mb-4">
-        We require this data to provide you with the goods or services offered by us including but not limited, for the
-        below set out purposes:
-      </p>
-
-      <ul className="list-disc list-inside mb-4">
-        <li>Internal record keeping.</li>
-        <li>For improving our products or services.</li>
-        <li>For providing updates to you regarding our products or services including any special offers.</li>
-        <li>To communicate information to you</li>
-        <li>For internal training and quality assurance purposes</li>
-      </ul>
-
-      <h2 className="text-2xl font-bold my-4">Who do we share your data with</h2>
-
-      <p className="mb-4">
-        We may share your information or data with:
-      </p>
-
-      <ul className="list-disc list-inside mb-4">
-        <li>
-          Third parties including our service providers in order to facilitate the provisions of goods or services to
-          you, carry out your requests, respond to your queries, fulfil your orders or for other operational and business
-          reasons.
-        </li>
-        <li>With our group companies (to the extent relevant)</li>
-        <li>Our auditors or advisors to the extent required by them for performing their services</li>
-        <li>
-          Governmental bodies, regulatory authorities, law enforcement authorities pursuant to our legal obligations or
-          compliance requirements.
-        </li>
-      </ul>
-
-      <h2 className="text-2xl font-bold my-4">How we use cookies</h2>
-
-      <p className="mb-4">
-        We use "cookies" to collect information and to better understand customer behaviour. You can instruct your
-        browser to refuse all cookies or to indicate when a cookie is being sent. However, if you do not accept cookies,
-        you may not be able to avail our goods or services to the full extent. We do not control the use of cookies by
-        third parties. The third party service providers have their own privacy policies addressing how they use such
-        information.
-      </p>
-
-      <h2 className="text-2xl font-bold my-4">Your rights relating to your data</h2>
-
-      <p className="mb-4">
-        <span className="font-bold">Right to Review -</span> You can review the data provided by you and can request us
-        to correct or amend such data (to the extent feasible, as determined by us). That said, we will not be
-        responsible for the authenticity of the data or information provided by you.
-      </p>
-
-      <p className="mb-4">
-        <span className="font-bold">Withdrawal of your Consent -</span> You can choose not to provide your data, at any
-        time while availing our goods or services or otherwise withdraw your consent provided to us earlier, in writing
-        to our email ID: <a href="mailto:Info@easyeduverse.com" className="text-blue-500">Info@easyeduverse.com</a> In the
-        event you choose to not provide or later withdraw your consent, we may not be able to provide you our services
-        or goods. Please note that these rights are subject to our compliance with applicable laws.
-      </p>
-
-      <h2 className="text-2xl font-bold my-4">How long will we retain your information or data?</h2>
-
-      <p className="mb-4">
-        We may retain your information or data (i) for as long as we are providing goods and services to you; and (ii)
-        as permitted under applicable law, we may also retain your data or information even after you terminate the
-        business relationship with us. However, we will process such information or data in accordance with applicable
-        laws and this Policy.
-      </p>
-
-      <h2 className="text-2xl font-bold my-4">Data Security</h2>
-
-      <p className="mb-4">
-        We will use commercially reasonable and legally required precautions to preserve the integrity and security of
-        your information and data.
-      </p>
-
-      <h2 className="text-2xl font-bold my-4">Queries/ Grievance Officer</h2>
-
-      <p className="mb-4">
-        For any queries, questions or grievances about this Policy, please contact us using the contact information
-        provided on this website.
-      </p>
-    </div>
+      <div className="container mx-auto mt-20 px-4">
+        <strong>Privacy Policy</strong> 
+        <p>
+          EASYED EDUVERSE PRIVATE LIMITED  built the EasyEd app as
+          an Ad Supported app. This SERVICE is provided by
+          EASYED EDUVERSE PRIVATE LIMITED  at no cost and is intended for use as
+          is.
+        </p> 
+        <p>
+          This page is used to inform visitors regarding our
+          policies with the collection, use, and disclosure of Personal
+          Information if anyone decided to use our Service.
+        </p> 
+        <p>
+          If you choose to use our Service, then you agree to
+          the collection and use of information in relation to this
+          policy. The Personal Information that we collect is
+          used for providing and improving the Service. We will not use or share your information with
+          anyone except as described in this Privacy Policy.
+        </p> 
+        <p>
+          The terms used in this Privacy Policy have the same meanings
+          as in our Terms and Conditions, which are accessible at
+          EasyEd unless otherwise defined in this Privacy Policy.
+        </p> 
+        <p><strong>Information Collection and Use</strong></p> 
+        <p>
+          For a better experience, while using our Service, we
+          may require you to provide us with certain personally
+          identifiable information. The information that
+          we request will be retained by us and used as described in this privacy policy.
+        </p> 
+        <div>
+          <p>
+          The app does use third-party services that may collect
+          information used to identify you.
+        </p> 
+        <p>
+            Link to the privacy policy of third-party service providers used
+            by the app
+          </p> 
+          <ul>
+            <li><a href="https://www.google.com/policies/privacy/" target="_blank" rel="noopener noreferrer">Google Play Services</a></li>
+            <li><a href="https://support.google.com/admob/answer/6128543?hl=en" target="_blank" rel="noopener noreferrer">AdMob</a></li>
+            </ul>
+            </div>
+            <p><strong>Log Data</strong></p> 
+            <p>
+          We want to inform you that whenever you
+          use our Service, in a case of an error in the app
+          we collect data and information (through third-party
+          products) on your phone called Log Data. This Log Data may
+          include information such as your device Internet Protocol
+          (“IP”) address, device name, operating system version, the
+          configuration of the app when utilizing our Service,
+          the time and date of your use of the Service, and other
+          statistics.
+        </p> <p><strong>Cookies</strong></p> <p>
+          Cookies are files with a small amount of data that are
+          commonly used as anonymous unique identifiers. These are sent
+          to your browser from the websites that you visit and are
+          stored on your device's internal memory.
+        </p> <p>
+          This Service does not use these “cookies” explicitly. However,
+          the app may use third-party code and libraries that use
+          “cookies” to collect information and improve their services.
+          You have the option to either accept or refuse these cookies
+          and know when a cookie is being sent to your device. If you
+          choose to refuse our cookies, you may not be able to use some
+          portions of this Service.
+        </p> <p><strong>Service Providers</strong></p> <p>
+          We may employ third-party companies and
+          individuals due to the following reasons:
+        </p> <ul><li>To facilitate our Service;</li> <li>To provide the Service on our behalf;</li> <li>To perform Service-related services; or</li> <li>To assist us in analyzing how our Service is used.</li></ul> <p>
+          We want to inform users of this Service
+          that these third parties have access to their Personal
+          Information. The reason is to perform the tasks assigned to
+          them on our behalf. However, they are obligated not to
+          disclose or use the information for any other purpose.
+        </p> <p><strong>Security</strong></p> <p>
+          We value your trust in providing us your
+          Personal Information, thus we are striving to use commercially
+          acceptable means of protecting it. But remember that no method
+          of transmission over the internet, or method of electronic
+          storage is 100% secure and reliable, and we cannot
+          guarantee its absolute security.
+        </p> <p><strong>Links to Other Sites</strong></p> <p>
+          This Service may contain links to other sites. If you click on
+          a third-party link, you will be directed to that site. Note
+          that these external sites are not operated by us.
+          Therefore, we strongly advise you to review the
+          Privacy Policy of these websites. We have
+          no control over and assume no responsibility for the content,
+          privacy policies, or practices of any third-party sites or
+          services.
+        </p> <p><strong>Childrens Privacy</strong></p> <div><p>
+          These Services do not address anyone under the age of 13.
+          We do not knowingly collect personally
+          identifiable information from children under 13 years of age. In the case
+          we discover that a child under 13 has provided
+          us with personal information, we immediately
+          delete this from our servers. If you are a parent or guardian
+          and you are aware that your child has provided us with
+          personal information, please contact us so that
+          we will be able to do the necessary actions.
+        </p></div><p><strong>Changes to This Privacy Policy</strong></p> <p>
+          We may update our Privacy Policy from
+          time to time. Thus, you are advised to review this page
+          periodically for any changes. We will
+          notify you of any changes by posting the new Privacy Policy on
+          this page.
+        </p> <p>This policy is effective as of 2024-01-14</p> <p><strong>Contact Us</strong></p> <p>
+          If you have any questions or suggestions about our
+          Privacy Policy, do not hesitate to contact us at easyededuverse@gmail.com.
+        </p> <p>This privacy policy page was created at <a href="https://privacypolicytemplate.net" target="_blank" rel="noopener noreferrer">privacypolicytemplate.net </a>and modified/generated by <a href="https://app-privacy-policy-generator.nisrulz.com/" target="_blank" rel="noopener noreferrer">App Privacy Policy Generator</a></p>
+      </div>
     </>
   );
 };
